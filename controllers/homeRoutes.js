a36480463e7b5efa1bc90036396af4f2824786d8
Aaron Cendejas <accenaar0@gmail.com>
2024-02-01 21:15:01 -0700

    added the push functionality to add username and password

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,22 +3,32 @@ const User = require('../models/User');
 
 router.get('/', async (req, res) => {
   // TODO: Render template with Sequelize data
- const userData = await User.findAll({
-    attribut: { exclude: ['password']}
+  const userData = await User.findAll({
+    attribut: { exclude: ['password'] }
   });
-  const users = userData.map(user => user.get({plain: true}));
+  const users = userData.map(user => user.get({ plain: true }));
   res.render('homepage', { users });
 });
 
 router.get('/users', async (req, res) => {
-    const userData = await User.findAll();
-    // console.log(userData);
-    res.json(userData);
+  const userData = await User.findAll();
+  // console.log(userData);
+  res.json(userData);
 })
 
-// router.post('/users', async (req, res) => {
-//     const userData = await User.findAll();
-//     res.json(userData);
-// })
+router.post('/users', (req, res) => {
+  const { name, email, password } = req.body;
+  User.create({
+    name: name,
+    email: email,
+    password: password
+  }).then(
+    newUser => { res.status(201).json(newUser) }
+  ).catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  })
+});
+
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -12,17 +12,15 @@ User.init(
   {
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      
       primaryKey: true,
       autoIncrement: true,
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
       unique: true,
       validate: {
         isEmail: true,
@@ -30,7 +28,6 @@ User.init(
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
       validate: {
         len: [8],
       },
