b41c603e862b68fdd204d4f24f0d2e8754a6c7dd
henrythedrip <146283648+henrythedrip@users.noreply.github.com>
2024-02-02 17:20:48 -0700

    attempting to make the delete functionality in sequelize and homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -30,5 +30,22 @@ router.post('/users', (req, res) => {
   })
 });
 
+// router.delete('/users:id', (req, res) => {
+//   User.destroy({
+//     where: { id: req.params.id }
+//   })
+//     .then(num => {
+//       if (num == 1) {
+//         res.send({ message : "user deleted sucessfully!"});
+//       } else {
+//         res.send({ message: `cannot delete user with id=${req.params.id}. maybe user was not found`});
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message: `error deleting id=${req.params.id}`
+//       });
+//     });
+// });
 
 module.exports = router;
