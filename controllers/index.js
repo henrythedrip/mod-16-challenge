e7411a5413a7a66f5d861d3da7aa6c02cd1b3906
henrythedrip <146283648+henrythedrip@users.noreply.github.com>
2024-02-09 14:46:02 -0700

    added some logout and login routes, and profile page

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
-
+//https://localhost:3001/
 router.use('/', homeRoutes);
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,24 @@
+// const { response } = require("express");
+
+console.log('hi this should work please');
 const loginFormHandler = async (event) => {
+
     event.preventDefault();
   
     // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('#login-email').value.trim();
+    const password = document.querySelector('#login-password').value.trim();
   
+    console.log(email, password);
+
     if (email && password) {
       // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
+
       if (response.ok) {
         // If successful, redirect the browser to the profile page
         document.location.replace('/profile');
@@ -21,7 +27,9 @@ const loginFormHandler = async (event) => {
       }
     }
   };
-  
+
+//   ----------------------------
+
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
@@ -45,10 +53,25 @@ const loginFormHandler = async (event) => {
   };
   
   document
-    .querySelector('.login-form')
+    .querySelector('.login-drip')
     .addEventListener('submit', loginFormHandler);
   
   document
-    .querySelector('.signup-form')
+    .querySelector('.create-account-drip')
     .addEventListener('submit', signupFormHandler);
   
+// DEBUG: add logout button that is NOT conditional
+const debugLogoutHandler = async event => {
+  await fetch('/logout', {
+    method: 'POST'
+  });
+  if (response.ok) {
+    document.location.replace('/login')
+  } else {
+    alert('failed to logout');
+  }
+}
+
+document
+  .querySelector('#debug-logout-button')
+  .addEventListener('click', debugLogoutHandler)
