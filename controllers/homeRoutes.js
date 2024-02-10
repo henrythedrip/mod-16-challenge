bac369eca0b77f969481d2e801c789f27c8da894
henrythedrip <146283648+henrythedrip@users.noreply.github.com>
2024-02-09 18:58:24 -0700

    changed the location of music videos from server.js to homeroutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -202,6 +202,28 @@ router.get("/contact", (req, res)=>{
   res.render('contact')
 });
 
+router.get('/music-videos', (req, res) => {
+  const videos = [
+    {
+      title: "Light Sounds (feat. UA The Duo)",
+      description: "A dynamic collaboration with UA The Duo",
+      embedUrl: "https://www.youtube.com/embed/g9ujuI1o8jM"
+    },
+    {
+      title: "Down to Ride",
+      description: "A catchy and irreverent song filmed at FICE Gallery.",
+      embedUrl: "https://www.youtube.com/embed/xae9OozYjxk"
+    },
+    {
+      title: "11:11",
+      description: "A psychedelic inter-galactic experience bruh",
+      embedUrl: "https://www.youtube.com/embed/nJy3iHPxIDo"
+    }
+  ];
+
+  // Render the music-videos.handlebars template with the videos data
+  res.render('music-videos', { videos });
+});
 // route to disallow anyone from seeing pages without logging in
 // router.get('/', withAuth, async (req, res) => {
 // });
--- server.js
+++ server.js
@@ -46,28 +46,28 @@ app.use(routes);
 
 
 // Define routes
-app.get('/music-videos', (req, res) => {
-  const videos = [
-    {
-      title: "Light Sounds (feat. UA The Duo)",
-      description: "A dynamic collaboration with UA The Duo",
-      embedUrl: "https://www.youtube.com/embed/g9ujuI1o8jM"
-    },
-    {
-      title: "Down to Ride",
-      description: "A catchy and irreverent song filmed at FICE Gallery.",
-      embedUrl: "https://www.youtube.com/embed/xae9OozYjxk"
-    },
-    {
-      title: "11:11",
-      description: "A psychedelic inter-galactic experience bruh",
-      embedUrl: "https://www.youtube.com/embed/nJy3iHPxIDo"
-    }
-  ];
+// app.get('/music-videos', (req, res) => {
+//   const videos = [
+//     {
+//       title: "Light Sounds (feat. UA The Duo)",
+//       description: "A dynamic collaboration with UA The Duo",
+//       embedUrl: "https://www.youtube.com/embed/g9ujuI1o8jM"
+//     },
+//     {
+//       title: "Down to Ride",
+//       description: "A catchy and irreverent song filmed at FICE Gallery.",
+//       embedUrl: "https://www.youtube.com/embed/xae9OozYjxk"
+//     },
+//     {
+//       title: "11:11",
+//       description: "A psychedelic inter-galactic experience bruh",
+//       embedUrl: "https://www.youtube.com/embed/nJy3iHPxIDo"
+//     }
+//   ];
 
-  // Render the music-videos.handlebars template with the videos data
-  res.render('music-videos', { videos });
-});
+//   // Render the music-videos.handlebars template with the videos data
+//   res.render('music-videos', { videos });
+// });
 
 // Add more routes as needed
 
